5bd8115aefe505bc5e426e524b89815f465ea98b
paxtaeo <paxtaeo@gmail.com>
2022-02-01 01:59:13 +0900

    Add kakaomap

--- feed/views.py
+++ feed/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from django.views.decorators.http import require_POST
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 from accounts.models import User
 from .models import Place, Review
@@ -56,6 +57,11 @@ class PlaceProfile(generic.DetailView):
 class Explore(LoginRequiredMixin, generic.ListView):
     template_name = 'feed/explore.html'
     context_object_name = 'review_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['KAKAOMAP_API_KEY'] = settings.KAKAOMAP_API_KEY
+        return context  
 
     def get_queryset(self):
         user = self.request.user

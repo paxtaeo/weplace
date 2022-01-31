c44ae6a35643588777ca6c3f8c1b2d5db4f08357
paxtaeo <paxtaeo@gmail.com>
2022-01-31 23:34:27 +0900

    Add index view

--- weplace/urls.py
+++ weplace/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import Index
 
 urlpatterns = [
+    path('', Index.as_view(), name='index'),
+
     path('admin/', admin.site.urls),
     path('feed/', include('feed.urls')),
 ]

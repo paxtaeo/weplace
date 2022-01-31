455ae9a9244785c37c1532fef815bcd7cd897eaa
paxtaeo <paxtaeo@gmail.com>
2022-01-31 14:32:47 +0900

    Add user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class MyUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom', {'fields': ('screen_name',)}),
+    )
+
+admin.site.register(User, MyUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,6 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    screen_name = models.CharField(max_length=150, blank=True)
+
